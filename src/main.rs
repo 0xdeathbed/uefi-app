2a9ed1615e12c524984c4f1ab9eb214ee15cbde8
Paras <161898004+paradoxedmind@users.noreply.github.com>
2024-06-02 17:52:53 +0530

    located ACPI tables

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,13 @@
 #![no_main] // disable entry function based on crt0
 #![feature(alloc_error_handler)]
 
+// link alloc crate
 extern crate alloc;
 
 use alloc::vec::Vec;
 use core::{alloc::Layout, fmt::Write};
 use log::info;
-use uefi::prelude::*;
-
-// #[panic_handler]
-// fn panic(_info: &PanicInfo) -> ! {
-//     loop {}
-// }
+use uefi::{prelude::*, table::cfg};
 
 /*
 https://uefi.org/specs/UEFI/2.10/04_EFI_System_Table.html
@@ -20,42 +16,20 @@ https://uefi.org/specs/UEFI/2.10/04_EFI_System_Table.html
 This is the main entry point for a UEFI Image. This entry point is the same for
 UEFI applications and UEFI drivers.
 
-typedef
-EFI_STATUS
-(EFIAPI *EFI_IMAGE_ENTRY_POINT) (
-  IN EFI_HANDLE                  ImageHandle,
-  IN EFI_SYSTEM_TABLE            *SystemTable
-  );
-
-ImageHandle
-The firmware allocated handle for the UEFI image.
-
-SystemTable
-A pointer to the EFI System Table.
-
 `efi_main` is common convention for UEFI applications and Rust compiler will look for it by default
 */
-// #[no_mangle]
-// pub extern "efiapi" fn efi_main(image: *mut c_void, system_table: *const c_void) -> usize {
-//     loop {}
-// }
-
 #[entry]
 fn efi_main(image: Handle, mut system_table: SystemTable<Boot>) -> Status {
     // initialize the allocator and loggings
     uefi::helpers::init(&mut system_table).unwrap();
-    // initialize the allocator
-    // unsafe {
-    //     uefi::allocator::init(&mut system_table);
-    // }
+    let sys_table = uefi::helpers::system_table();
 
     let stdout = system_table.stdout();
     stdout.clear().unwrap();
     writeln!(stdout, "Hello World!").unwrap();
 
-    // let rev = system_table.uefi_revision();
-    // info!("UEFI {}.{}", rev.major(), rev.minor());
-    // system_table.boot_services().stall(10_000_000); // Stall for 10 seconds
+    let rev = sys_table.uefi_revision();
+    info!("UEFI {}.{}", rev.major(), rev.minor());
 
     // Using the allocator
     let mut v = Vec::new();
@@ -63,6 +37,15 @@ fn efi_main(image: Handle, mut system_table: SystemTable<Boot>) -> Status {
     v.push(2);
     // info!("v = {v:?}");
     writeln!(stdout, "v = {v:?}").unwrap();
+
+    let config_entries = sys_table.config_table();
+    // adddress of relevant ACPI Root System Description table
+    let rsdp_addr = config_entries
+        .iter()
+        .find(|&e| matches!(e.guid, cfg::ACPI_GUID | cfg::ACPI2_GUID))
+        .map(|e| e.address);
+
+    writeln!(stdout, "rsdp addr: {:?}", rsdp_addr).unwrap();
 
     loop {}
 }
