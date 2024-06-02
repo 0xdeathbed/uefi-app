10bbc57f09fd6d76b881cc75cf81cbd41d37d0fe
Paras <161898004+paradoxedmind@users.noreply.github.com>
2024-06-02 17:29:28 +0530

    memory allocations

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 
 ```bash
-cargo build --target x86_64-unknown-uefi -Z build-std=core \
+cargo build --target x86_64-unknown-uefi -Z build-std=core,alloc \
     -Z build-std-features=compiler-builtins-mem
 cargo run --package disk_image -- target/x86_64-unknown-uefi/debug/uefi-app.efi
 qemu-system-x86_64 \
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std] // disable linking standard library
 #![no_main] // disable entry function based on crt0
+#![feature(alloc_error_handler)]
 
-use core::fmt::Write;
+extern crate alloc;
+
+use alloc::vec::Vec;
+use core::{alloc::Layout, fmt::Write};
 use log::info;
 use uefi::prelude::*;
 
@@ -38,15 +42,34 @@ A pointer to the EFI System Table.
 
 #[entry]
 fn efi_main(image: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    // initialize the allocator and loggings
     uefi::helpers::init(&mut system_table).unwrap();
+    // initialize the allocator
+    // unsafe {
+    //     uefi::allocator::init(&mut system_table);
+    // }
 
     let stdout = system_table.stdout();
     stdout.clear().unwrap();
     writeln!(stdout, "Hello World!").unwrap();
 
-    let rev = system_table.uefi_revision();
-    info!("UEFI {}.{}", rev.major(), rev.minor());
+    // let rev = system_table.uefi_revision();
+    // info!("UEFI {}.{}", rev.major(), rev.minor());
     // system_table.boot_services().stall(10_000_000); // Stall for 10 seconds
 
+    // Using the allocator
+    let mut v = Vec::new();
+    v.push(1);
+    v.push(2);
+    // info!("v = {v:?}");
+    writeln!(stdout, "v = {v:?}").unwrap();
+
     loop {}
+}
+
+/// This function is called when an allocation fails,
+/// typically because the system is out of memory
+#[alloc_error_handler]
+fn alloc_error(_layout: Layout) -> ! {
+    panic!("out of memory")
 }
