fb04d52883c102491998e72b7c1aa484364a3f9b
Paras <161898004+paradoxedmind@users.noreply.github.com>
2024-06-03 14:54:39 +0530

    physical memory map

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ extern crate alloc;
 use alloc::vec::Vec;
 use core::{alloc::Layout, fmt::Write};
 use log::info;
-use uefi::{prelude::*, proto::console::gop::GraphicsOutput, table::cfg};
+use uefi::{
+    prelude::*,
+    proto::{console::gop::GraphicsOutput, loaded_image::LoadedImage},
+    table::{boot::MemoryType, cfg},
+};
 
 /*
 https://uefi.org/specs/UEFI/2.10/04_EFI_System_Table.html
@@ -46,7 +50,10 @@ fn efi_main(image: Handle, mut system_table: SystemTable<Boot>) -> Status {
         .map(|e| e.address);
 
     writeln!(stdout, "rsdp addr: {:?}", rsdp_addr).unwrap();
+
+    // Graphic Output Protocol for writing
     let bt = sys_table.boot_services();
+
     let gop_handle = bt.get_handle_for_protocol::<GraphicsOutput>().unwrap();
     let mut gop = bt
         .open_protocol_exclusive::<GraphicsOutput>(gop_handle)
@@ -59,6 +66,20 @@ fn efi_main(image: Handle, mut system_table: SystemTable<Boot>) -> Status {
         gop.frame_buffer().as_mut_ptr()
     )
     .unwrap();
+
+    // // estimated map size
+    // let mmap_size = bt.memory_map_size();
+    // let max_mmap_size = mmap_size.map_size + 8 * mem::size_of::<MemoryDescriptor>();
+    // let mut buffer: Vec<u8> = Vec::with_capacity(max_mmap_size);
+    // unsafe {
+    //     buffer.set_len(max_mmap_size);
+    // }
+
+    // let mmap_storage = bt.memory_map(&mut buffer).unwrap();
+
+    uefi::allocator::exit_boot_services();
+    let (sys_table, mem_map) =
+        uefi::helpers::system_table().exit_boot_services(MemoryType::LOADER_DATA);
 
     loop {}
 }
